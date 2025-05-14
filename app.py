d504dc9a5fb8fcc33b2bbaa66a9aa3cd2a99699b
Johan <johancortes819@gmail.com>
2025-05-14 16:43:20 -0500

    A

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, redirect, url_for
 import pandas as pd
 import random
 
-
 app = Flask(__name__)
 
 if __name__ == '_main_':
@@ -11,6 +10,20 @@ if __name__ == '_main_':
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/entendimiento-negocio')
+def entendimiento_negocio():
+    return render_template('entendimiento_negocio.html')
+
+@app.route('/entendimiento-datos')
+def entendimiento_datos():
+    return render_template('entendimiento_datos.html')
+
+@app.route('/ingenieria-datos')
+def ingenieria_datos():
+    return render_template('ingenieria_datos.html')
+
 @app.route('/ingresar')
-def Ingresar():
+def ingresar():
     return render_template('ingresar.html')
+
